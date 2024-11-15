4867efd221144aa81238f63a2a37823a1fb749ba
MishraAbhishek07 <davmishra96@gmail.com>
2024-11-15 16:42:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
 //your JS code here. If required.
- const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 
-                      'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 
-                      'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 
-                      'An Old Dog'];
+ //your JS code here. If required.
+let list = document.getElementById("band")
 
-        // Words to ignore while sorting
-        const ignoreWords = ["a", "an", "the"];
+const bands = [
+    'Anywhere But Here',
+    'The Bled',
+    'Counterparts',
+    'The Devil Wears Prada',
+    'The Midway State',
+    'Norma Jean',
+    'Oh, Sleeper',
+    'An Old Dog',
+    'Pierce the Veil',
+    'The Plot in You',
+    'Say Anything',
+    'A Skylit Drive',
+    'We Came as Romans'
+  ]
+  
 
-        // Function to sort band names
-        function sortBands(bands) {
-            return bands.sort((a, b) => {
-                const bandA = a.split(' ').filter(word => !ignoreWords.includes(word.toLowerCase())).join(' ');
-                const bandB = b.split(' ').filter(word => !ignoreWords.includes(word.toLowerCase())).join(' ');
 
-                return bandA.localeCompare(bandB);
-            });
-        }
+  for(let item of bands)
+  {
+    let listItm = document.createElement("li")
+    listItm.innerText = item
+	      list.setAttribute("id", "bands")
 
-        // Display sorted bands in the unordered list
-        function displaySortedBands() {
-            const sortedBands = sortBands(bands);
-            const bandList = document.getElementById('band');
-            
-            sortedBands.forEach(band => {
-                const li = document.createElement('li');
-                li.textContent = band;
-                bandList.appendChild(li);
-            });
-        }
-
-        // Call the function to display the sorted bands
-        displaySortedBands();
+    list.appendChild(listItm)
+  }
